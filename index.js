543266a6ba2ea8f680ef162c1c749a5a79562f42
JackLash09 <jacquesboudreau01@gmail.com>
2023-08-21 13:02:39 +0200

    fixed pointer to frontend

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 
 app.use(cors())
-app.use(express.static('dist'))
+app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
